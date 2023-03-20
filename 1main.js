ea1b241f907c8b791c069b56497d3d8145753d10
Shwetha <104769790+Shwetha2008@users.noreply.github.com>
2023-03-20 18:40:10 +0530

    Add files via upload

---
+++ 1main.js
@@ -0,0 +1,20 @@
+function next()
+{
+    window.location = "2Bed.html";
+}
+function next1()
+{
+    window.location = "bottle.html";
+}
+function next2()
+{
+    window.location = "fruits.html";
+}
+function next3()
+{
+    window.location = "laptop.html";
+}
+function next4()
+{
+    window.location = "cup.html";
+}
---
+++ fruits.js
@@ -0,0 +1,39 @@
+img =" ";
+status = " ";
+
+function preload()
+{
+    img = loadImage("fruit_basket.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(500,420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+}
+function draw()
+{
+    image(img, 0, 0, 500, 420);
+}
+
